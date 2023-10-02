249bd0c55858fa72d266567d6e3c107040021c94
matteobnvl <matteobonneval19@gmail.com>
2023-10-02 18:48:04 +0200

    initialization

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 cd api_troc_troc
+git pull
+php bin/console d:m:m
 echo "Démarrage du serveur API..."
 symfony server:start &
 
 cd ../front_troc_troc
+git pull
 echo "Démarrage du serveur Front..."
 npm run dev &
 
