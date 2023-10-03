6510706369a919b8c85ece438b83032e594e69fb
Darren Ndouongo <darrenndouongo@gmail.com>
2023-10-03 15:42:52 +0200

    Ajout de la page connexion avec deux inputs et un bouton se connecter

---
+++ front_troc_troc/src/components/Containeurfichier.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>test</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Y',
+});
+
+
+</script>
---
+++ front_troc_troc/src/views/LoginView.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="form">
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6" flex="center">
+        <v-card>
+          <v-card-title>
+            <h3>Connexion</h3>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field 
+                id="emmail"
+                v-model="firstName"
+                label="E-mail"
+                type="email"
+                required
+              ></v-text-field>
+              <v-text-field 
+                id="mdp"
+                v-model="email"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-btn color="primary" @click="register">Se Connecter</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</div>
+       
+</template>
+
+<style>
+
+#form {
+  width: 140%;
+  margin-top: 12%;
+}
+
+
+</style>
+
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import Bloc from '../components/Containeurfichier.vue';
+
+export default defineComponent({
+  name: 'Y',
+
+  components: {
+    Bloc,
+  },
+});
+</script>
