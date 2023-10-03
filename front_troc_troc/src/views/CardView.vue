03e96652b8675530f0f68ff5aa7b5a2ce1d2c41e
unknown <coco40090@gmail.com>
2023-10-03 10:40:23 +0200

    ajout Cardview

---
+++ front_troc_troc/src/views/CardView.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://www.bing.com/th?id=OADD2.7559517640938_19VP1RRFJO3U0L26DM&pid=21.2&c=16&roil=0.1206&roit=0.2418&roir=0.894&roib=0.8491&w=472&h=247&rs=2"
+      height="200px"
+      cover
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange-lighten-2"
+        variant="text"
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="show = !show"
+      ></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
